50079eb8e761040e7e205036f2bec3722775137c
unknown <20230450+HDR@users.noreply.github.com>
2020-01-13 22:30:45 +0100

    Fix an issue preventing compiling under linux

--- src/const.h
+++ src/const.h
@@ -3,7 +3,7 @@
 
 #define VER "1.0 (64bit)"
 
-enum error_t
+enum error_codes
 { TIMEOUT = -1, FILEERROR_O = -2, FILEERROR_W = -3, FILEERROR_R =
     -4, SEND_ERROR = -5,
   BAD_PACKET = -6, BAD_PARAMS = -7, PORT_ERROR = -8, WRONG_SIZE = -9
